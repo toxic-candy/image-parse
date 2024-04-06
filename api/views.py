3b5ca7559790e9b05c341e51a82dcc6b75dce2c8
toxic-candy <yaashvinsv@gmail.com>
2024-04-06 15:36:13 +0530

    Initial commit

---
+++ api/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from PIL import Image
+import os
+import io
+import base64
+from django.shortcuts import render
+from django.utils.safestring import mark_safe
+
+@api_view(['GET'])
+def getData(request):
+
+    # with open("kat.jpg", "rb") as imagefile:
+    #     convert = base64.b64encode(imagefile.read())
+    
+    img = Image.open('kat.jpg')
+
+
+    # return Response(convert.decode('utf-8'))
+    return Response(img.show())
